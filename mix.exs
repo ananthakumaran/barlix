fc5bd0c3cb593174c2612898cdaff06143acdfd7
Andreas Lappe <a.lappe@zimmer7.com>
2021-03-10 11:57:40 +0100

    Add dialyxir

--- mix.exs
+++ mix.exs
@@ -25,8 +25,9 @@ defmodule Barlix.Mixfile do
   defp deps do
     [
       {:png, "~> 0.2"},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.20.2", only: :dev},
-      {:mix_test_watch, "~> 0.2", only: :dev},
+      # {:mix_test_watch, "~> 0.2", only: :dev},
       {:tempfile, "~> 0.1.0", only: :test},
       {:excheck, "~> 0.6.0", only: :test},
       {:triq, "~> 1.3", only: :test}
